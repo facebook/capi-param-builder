6b561096bbe72d99adc13615267172ae58ec7451
Hongjin Zhu <hongjinz@meta.com>
2025-07-25 19:00:22 -0700

    Java: downgrade Spring from example to compatible with Java 8
    Summary:
    - Add CI on folder level to make sure the demo, SDK are fit for Java 8
    - Downgrade Spring from 3.2.5 -> 2.7.x to be compatible with Java 8
      - Change API usage from jakarta to javax for Spring 2.7.x
    
    Differential Revision: D79024241
    
    fbshipit-source-id: 9349961cb23f394184c7e6444fd34bc746772186

--- java/example/src/main/java/springboot/filter/ParamBuilderFilter.java
+++ java/example/src/main/java/springboot/filter/ParamBuilderFilter.java
@@ -9,19 +9,19 @@ package com.example.springboot.filter;
 
 import com.facebook.capi.sdk.ParamBuilder;
 import com.facebook.capi.sdk.model.CookieSetting;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 // @Component
 public class ParamBuilderFilter implements Filter {
