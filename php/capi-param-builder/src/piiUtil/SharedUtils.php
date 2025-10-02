1f830ea59bc4f42571d03b0f34d0ad666a368d5a
Jiaming You <jiamingyou@meta.com>
2025-10-01 23:03:47 -0700

    Added StringUtils.php
    Summary: Added StringUtils.php which include various string normalization methods
    
    Differential Revision: D83722865
    
    fbshipit-source-id: 032e2e6481c4de81620a46fd86a5ffa243ec63a6

--- php/capi-param-builder/src/piiUtil/SharedUtils.php
+++ php/capi-param-builder/src/piiUtil/SharedUtils.php
@@ -10,10 +10,56 @@ namespace FacebookAds;
 
 class SharedUtils
 {
+
+  // Regex patterns (delimiters are /, and 'u' for unicode where needed)
+  const STRIP_WHITESPACE_REGEX = '/\s+/u';
+  // Punctuation characters: !"#$%&'()*+,-./:;<=>?@ [\]^_`{|}~
+  const STRIP_WHITESPACE_AND_PUNCTUATION_REGEX =
+  '/[!"#\$%&\'\(\)\*\+,\-\.\/:;<=>\?@ \[\\\\\]\^_`\{\|\}~\s]+/u';
+
+  const STRIP_NON_LATIN_ALPHA_NUMERIC_REGEX = '/[^a-zA-Z0-9]+/u';
+
   public static function looksLikeHashed($input)
   {
     // it could be sha256 or md5
     return is_string($input) && (preg_match('/^[A-Fa-f0-9]{64}$/', $input) ||
       preg_match('/^[a-f0-9]{32}$/', $input));
+  }
+
+  /**
+   * Strips characters from a string based on the mode.
+   *
+   * @param mixed $obj  The input to strip (only strings are processed).
+   * @param string $mode  One of: 'whitespace_only',
+   *   'whitespace_and_punctuation', 'all_non_latin_alpha_numeric'
+   * @return string
+
+   */
+  public static function strip($obj, $mode = 'whitespace_only')
+  {
+    $result = '';
+    if (is_string($obj)) {
+      switch ($mode) {
+        case 'whitespace_only':
+          $result = preg_replace(SharedUtils::STRIP_WHITESPACE_REGEX, '', $obj);
+          break;
+        case 'whitespace_and_punctuation':
+          $result = preg_replace(
+            SharedUtils::STRIP_WHITESPACE_AND_PUNCTUATION_REGEX,
+            '',
+            $obj
+          );
+          break;
+
+        case 'all_non_latin_alpha_numeric':
+          $result = preg_replace(
+            SharedUtils::STRIP_NON_LATIN_ALPHA_NUMERIC_REGEX,
+            '',
+            $obj
+          );
+          break;
+      }
+    }
+    return $result;
   }
 }
